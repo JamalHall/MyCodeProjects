cbd0aa0311f6d02069d552eef52d1e53b3174e29
JHall <JamalHallWebDev@gmail.com>
2021-08-31 16:23:26 -0400

    final

--- CodeWars/Enough is enough array values/main.js
+++ CodeWars/Enough is enough array values/main.js
@@ -3,24 +3,43 @@
 // deleteNth([1,1,3,3,7,2,2,2,2], 3) 
 //   //[1, 1, 3, 3, 7, 2, 2, 2])
 
-function deleteNth(arr,n){  
-  
-    let countArray = arr
-    let memoryMap = arr
-    let arrOfArr = []
-  
-    for(let i=0; i<arr.length-1; i++){ 
-     let element = memoryMap[i]
-     let ecount = countArray.filter((e,i) => e===element).length
-     console.log(element)
-     console.log(ecount)
-     arrOfArr.push([element,ecount])
-         
+function deleteNth(arr,n){ 
+    let newArr = [];
+for (let i = 0; i < arr.length; i++) {
+      if ( dupCount(arr[i]) < n ) {  //recursive call to the dupCount function
+        newArr.push(arr[i])
+      }
     }
-    console.log(arrOfArr)
-  
-  }    
-    deleteNth([20,37,20,21], 1)
-  
-  
-   
+    
+    function dupCount(item) {
+      let count = 0      
+      for (let i = 0; i < newArr.length; i++) {
+        if (item === newArr[i]) {
+          count++
+        }
+      }
+      return count
+    }
+  return console.log(newArr)
+    
+  } 
+
+  deleteNth([20,37,20,21], 1)
+
+
+
+
+
+//  let countArray = arr
+//   let memoryMap = arr
+//   let arrOfArr = []
+
+//   for(let i=0; i<arr.length-1; i++){ 
+//    let element = memoryMap[i]
+//    let ecount = countArray.filter((e,i) => e===element).length
+//    console.log(element)
+//    console.log(ecount)
+//    arrOfArr.push([element,ecount])
+       
+//   }
+//   console.log(arrOfArr)
