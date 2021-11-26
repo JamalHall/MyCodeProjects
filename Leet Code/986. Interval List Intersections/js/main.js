8da10a200a9790b42eeb3046d2001db1caa59c35
JamalHall <JamalHallWebDev@gmail.com>
2021-11-25 22:38:37 -0500

    Update main.js

--- Leet Code/986. Interval List Intersections/js/main.js
+++ Leet Code/986. Interval List Intersections/js/main.js
@@ -42,6 +42,7 @@ let range2=[]
 for(e1 of firstList){
 for(e2 of secondList){
   console.log(e1[0],e2[0])
+  if(e2[0]<=e1[0]) out.push(e1[0])
  
 }  
 } 
@@ -51,8 +52,8 @@ console.log(out)
 
 }
 
-//console.log(intervalIntersection([[1,7]],[[3,10]]),'Output', [[3,7]])
+console.log(intervalIntersection([[1,7]],[[3,10]]),'Output', [[3,7]])
 
-console.log(intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]),'Output', [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]])
+//console.log(intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]),'Output', [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]])
 
 //console.log(intervalIntersection([[1,3],[5,9]],[]),'Output', [])
