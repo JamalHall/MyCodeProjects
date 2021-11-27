36a0bd4f451eb4e327ce36cf87a21d947d65af26
Jamal Hall <jamalhallwebdev@gmail.com>
2021-11-26 20:53:37 -0500

    2 solutions

--- Leet Code/986. Interval List Intersections/js/main.js
+++ Leet Code/986. Interval List Intersections/js/main.js
@@ -36,24 +36,44 @@ endj < startj+1
 */
 
 var intervalIntersection = function(firstList, secondList) {
-let out=[]
-let range1=[]
-let range2=[]
-for(e1 of firstList){
-for(e2 of secondList){
-  console.log(e1[0],e2[0])
-  if(e2[0]<=e1[0]) out.push(e1[0])
- 
-}  
-} 
-
-console.log(out)    
 
 
 }
 
-console.log(intervalIntersection([[1,7]],[[3,10]]),'Output', [[3,7]])
+//console.log(intervalIntersection([[1,7]],[[3,10]]),'Output', [[3,7]])
 
-//console.log(intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]),'Output', [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]])
+console.log(intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]),'Output', [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]])
 
 //console.log(intervalIntersection([[1,3],[5,9]],[]),'Output', [])
+
+
+
+// var intervalIntersection = function(firstList, secondList) {
+//   let out=[]
+//   for(eone of firstList){
+//     let ione=0 
+//     out[ione]=new Array(2)  
+//   for(etwo of secondList){
+//     let itwo=0
+//       let start1 = eone[0]
+//       let end1 = eone[1]
+//       let start2 = etwo[0]
+//       let end2 = etwo[1]
+//       console.log(start1,end1)
+//       console.log(start2,end2)
+//         //[biggest, smallest] use switch case statement and continues
+//       if(start1>=start2&&start1<=end2){
+//         //out[ione][itwo]=start1
+//       } else {
+//         //out[ione][itwo]=start2
+//       } 
+//       ione++
+//       itwo++  
+//      }
+//   console.log(out)
+//   }  
+  
+  
+//   return(out)    
+  
+//   }
