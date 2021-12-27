f5aae5ed36d44d1daf719c90dbc0caa21f7f9f06
Jamal Hall <jamalhallwebdev@gmail.com>
2021-12-27 09:13:51 -0500

    Update main.js

--- MegaSummerDaily/8-27/js/main.js
+++ MegaSummerDaily/8-27/js/main.js
@@ -69,10 +69,10 @@ function fib(n){
 
 
 // // L#100devs Solution
-// function fib(n){
-//   let arr=[0,1]
-//   for(let i=2; i<=n; i++){
-//   arr[i] = arr[i-1] +arr[i-2]
-//   }
-//   return arr[n]
-// }
+function fib(n){
+  let arr=[0,1]
+  for(let i=2; i<=n; i++){
+  arr[i] = arr[i-1] +arr[i-2]
+  }
+  return arr[n]
+}
