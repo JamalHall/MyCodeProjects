b1f3c1634f4f8ac7408646d662d4622e5559745a
Jamal Hall <jamalhallwebdev@gmail.com>
2022-02-06 19:08:11 -0500

    starting with auth0

---
+++ Web app and API authentication auth0-app/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+app.set('view engine', 'ejs')
+const PORT = process.env.PORT || 3000
+dotenv.config({path: './config/.env'})
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const indexRoute= require('./routes/route.js')
+
+app.use('/',indexRoute)
+
+app.listen(PORT, () => { 
+    console.log(`app.js is running on ${PORT} ^c to stop`)
+})
+
