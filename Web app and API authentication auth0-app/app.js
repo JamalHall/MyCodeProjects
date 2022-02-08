c96f1a39b6f5ba2dc819e35420c7acb427105ace
JamalHall <JamalHallWebDev@gmail.com>
2022-02-07 19:28:44 -0500

    updates

--- Web app and API authentication auth0-app/app.js
+++ Web app and API authentication auth0-app/app.js
@@ -4,13 +4,20 @@ const dotenv = require('dotenv')
 app.set('view engine', 'ejs')
 const PORT = process.env.PORT || 3000
 dotenv.config({path: './config/.env'})
+
+const { auth } = require('express-openid-connect');
+
+const authConfig = require('./config/authCFG.js')
+app.use(auth(authConfig))
+
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+
 const indexRoute= require('./routes/route.js')
 
-app.use('/',indexRoute)
+app.use('/', indexRoute)
 
 app.listen(PORT, () => { 
     console.log(`app.js is running on ${PORT} ^c to stop`)
--- Web app and API authentication auth0-app/routes/route.js
+++ Web app and API authentication auth0-app/routes/route.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const { requiresAuth } = require('express-openid-connect')
 
 router.get('/', (req , res) => {
-    res.render('index', {title: 'Home'})
+    console.log(req.oidc.isAuthenticated())
+    res.render('index', {
+        title: 'Home un-secured page',
+        authStatus: req.oidc.isAuthenticated(),
+        user: req.oidc.user,
+    })
+})
+
+router.get('/secured', requiresAuth(), (req , res) => {
+    console.log(req.oidc.isAuthenticated(), req.oidc.user)
+    res.render('securedPage', {
+        title: 'Secure Page authenticated page',
+        authStatus: req.oidc.isAuthenticated(),
+        user: req.oidc.user,
+    })
+})
+
+router.get('/profile', requiresAuth(), (req , res) => {
+    console.log(req.oidc.isAuthenticated(), req.oidc.user)
+    res.render('profile', {
+            userProfile: req.oidc.user,
+            authStatus: req.oidc.isAuthenticated(),
+    })
 })
 
 module.exports = router
