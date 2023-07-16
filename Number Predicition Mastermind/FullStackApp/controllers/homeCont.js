48c60d20cf415fa1de08684b4c4a50eb9366b69b
JHall <JamalHallWebDev@gmail.com>
2023-07-16 11:39:01 -0400

    updates

--- Number Predicition Mastermind/FullStackApp/controllers/homeCont.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    showHome: (req,res) => {
-        console.log(req.oidc.isAuthenticated(), req.oidc.user)
-        res.render('index.ejs',{
-            authStatus: req.oidc.isAuthenticated(),
-            user: req.oidc.user,
-        })
-    }
-}
